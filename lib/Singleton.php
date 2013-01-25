0b193c7962a8c24fe501719b77547325132fd9e3
Edmond Kachale <kachaleedmond@gmail.com>
2013-01-25 09:04:05 +0200

    Added instantiation method that restricts instances of a class to one object

---
+++ lib/Singleton.php
@@ -0,0 +1,37 @@
+<?php
+/******************************************************************************
+ *      Singleton.php                                                         *
+ *      - Implements a singleton pattern                                      *
+ *                                                                            *
+ *      https://github.com/ceekays/dbconnect                                  *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created on January 25, 2013                                           *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+
+abstract class Singleton {
+
+//singleton instance
+  private static $instances = array();
+
+//to prevent external instantiation
+  private function __construct() {}
+
+//instance method
+  final public static function instantiate() {
+
+    $called_class = get_called_class();
+
+    if (!isset(self::$instances[$called_class]))
+      self::$instances[$called_class] = $called_class;
+
+    return self::$instances[$called_class];
+  }
+}
+
+?>
+
